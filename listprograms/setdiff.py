519dadb28b68c5d80bd390a6412446669c85269a
aswathi <aswathisanthosh085@gmail.com>
2020-12-02 09:20:35 +0530

    assignment on 1-12-2020

---
+++ listprograms/setdiff.py
@@ -0,0 +1,14 @@
+# st={1,2,3,4,5,6}
+# st1={5,6,7,8,9}
+# print("fierst set before difference :",st)
+# st=st.difference(st1)
+# print("set after operation :",st)
+
+
+# students failed passed
+
+stud={"a","b","c","d","e","f"}
+passed={"a","c","e",}
+print("passed students:",passed)
+failed=stud.difference(passed)
+print()
